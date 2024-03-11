0c8686698465ea99b4320da1633c7e0fc38a1529
Soumajyoti <soumyabwn3@gmail.com>
2024-03-11 12:31:21 +0530

    final commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, 'src')));
+// app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static("src"))
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname })
